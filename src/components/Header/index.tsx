eab4f7b77d44e4c3c4738542863321c99d213ead
Felipe Fadoni <felipefadonimt@gmail.com>
2022-05-05 21:50:44 -0300

    feat: header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,41 @@
+import logoWebfadoni from "@/assets/logo-webfadoni.svg";
+import Image from "next/image";
+import Link from "next/link";
+import { MdSearch } from "react-icons/md";
+import { RiMenu5Line } from "react-icons/ri";
 import { HeaderContainer } from "./styles";
 
 export default function Header() {
   return (
     <HeaderContainer>
-      <div className="center-header">asdasdasd</div>
+      <div className="center-header">
+        <div className="header-left">
+          <button title="Menu">
+            <RiMenu5Line size={28} />
+          </button>
+          <div className="logo">
+            <Link href="/">
+              <a title="Voltar para o início">
+                <Image
+                  src={logoWebfadoni}
+                  alt="WebFadoni"
+                  width={163}
+                  height={48}
+                  loading="lazy"
+                />
+              </a>
+            </Link>
+          </div>
+        </div>
+        <div className="header-right">
+          <button title="Buscar">
+            <MdSearch size={32} />
+          </button>
+          <div className="search">
+            <input type="text" placeholder="Buscar" />
+          </div>
+        </div>
+      </div>
     </HeaderContainer>
   );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -4,12 +4,82 @@ export const HeaderContainer = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100%;
-  background-color: red;
+  width: calc(100% - 32px);
+  padding: 16px;
 
   .center-header {
     width: 1110px;
-    display: block;
+    display: flex;
     margin: 0 auto;
+    justify-content: space-between;
+
+    .header-left {
+      display: flex;
+      align-items: center;
+
+      button {
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+        svg {
+          color: #fff;
+          transition: all 0.2s;
+        }
+        &:hover {
+          svg {
+            transform: scale(1.3);
+          }
+        }
+      }
+
+      .logo {
+        margin-left: 8px;
+      }
+    }
+
+    .header-right {
+      display: flex;
+      align-items: center;
+
+      button {
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+        svg {
+          color: rgba(255, 255, 255, 0.5);
+          transition: all 0.2s;
+        }
+        :hover {
+          svg {
+            transform: scale(1.3);
+            color: #fff;
+          }
+        }
+      }
+
+      .search {
+        margin-left: 8px;
+
+        input {
+          background: transparent;
+          padding: 8px;
+          font-size: 1rem;
+          color: rgba(255, 255, 255, 0.5);
+          border-radius: 4px;
+          width: 0px;
+          transition: all 0.3s;
+        }
+      }
+
+      &:hover {
+        .search {
+          input {
+            width: 320px;
+            padding: 8px;
+            background-color: rgba(255, 255, 255, 0.2);
+          }
+        }
+      }
+    }
   }
 `;
