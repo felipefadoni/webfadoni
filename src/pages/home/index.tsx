45e3d629337e9a36a4cb4aefc4e6d81f108cc9ee
Felipe Fadoni <felipefadonimt@gmail.com>
2022-05-27 18:50:12 -0300

    feat: update pages

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,8 +3,70 @@ import { Container, Grid } from "@mui/material";
 import type { NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
-import news from "./initial-state";
-import { ContainerDestaques } from "./styles";
+import { ContainerDestaques } from "../../styles/home-styles";
+
+const news = [
+  {
+    url: "/noticia/teste-01",
+    titulo: "Google lança 3 novos cursos com certificados profissionalizantes",
+    time: "04/05/2022 - 13:19",
+  },
+  {
+    url: "/noticia/teste-02",
+    titulo: "Google lança 3 novos cursos com certificados profissionalizantes",
+    time: "04/05/2022 - 13:19",
+  },
+  {
+    url: "/noticia/teste-03",
+    titulo: "Google lança 3 novos cursos com certificados profissionalizantes",
+    time: "04/05/2022 - 13:19",
+  },
+  {
+    url: "/noticia/teste-04",
+    titulo: "Google lança 3 novos cursos com certificados profissionalizantes",
+    time: "04/05/2022 - 13:19",
+  },
+  {
+    url: "/noticia/teste-05",
+    titulo: "Google lança 3 novos cursos com certificados profissionalizantes",
+    time: "04/05/2022 - 13:19",
+  },
+  {
+    url: "/noticia/teste-06",
+    titulo: "Google lança 3 novos cursos com certificados profissionalizantes",
+    time: "04/05/2022 - 13:19",
+  },
+  {
+    url: "/noticia/teste-01",
+    titulo: "Google lança 3 novos cursos com certificados profissionalizantes",
+    time: "04/05/2022 - 13:19",
+  },
+  {
+    url: "/noticia/teste-02",
+    titulo: "Google lança 3 novos cursos com certificados profissionalizantes",
+    time: "04/05/2022 - 13:19",
+  },
+  {
+    url: "/noticia/teste-03",
+    titulo: "Google lança 3 novos cursos com certificados profissionalizantes",
+    time: "04/05/2022 - 13:19",
+  },
+  {
+    url: "/noticia/teste-04",
+    titulo: "Google lança 3 novos cursos com certificados profissionalizantes",
+    time: "04/05/2022 - 13:19",
+  },
+  {
+    url: "/noticia/teste-05",
+    titulo: "Google lança 3 novos cursos com certificados profissionalizantes",
+    time: "04/05/2022 - 13:19",
+  },
+  {
+    url: "/noticia/teste-06",
+    titulo: "Google lança 3 novos cursos com certificados profissionalizantes",
+    time: "04/05/2022 - 13:19",
+  },
+];
 
 const Home: NextPage = () => {
   return (
