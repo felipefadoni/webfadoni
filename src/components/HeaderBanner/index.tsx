92369ddc9fd027b214410c9fb7ea8334b5d348e9
Felipe Fadoni <felipefadonimt@gmail.com>
2022-06-27 12:59:14 -0300

    feat: ajuste sites imagens

--- src/components/HeaderBanner/index.tsx
+++ src/components/HeaderBanner/index.tsx
@@ -1,9 +1,12 @@
-import styles from "./header-banner.module.scss";
+import styles from './header-banner.module.scss';
 
 export default function HeaderBanner() {
-  return (
-    <div className={styles.bannerHeader}>
-      <div className={styles.headerBannerCenter}>728x90</div>
-    </div>
-  );
+  const disable = false;
+
+  if (disable)
+    return (
+      <div className={styles.bannerHeader}>
+        <div className={styles.headerBannerCenter}>728x90</div>
+      </div>
+    );
 }
